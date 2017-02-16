e8a00b6431d141a0c5e27314e5a2a989eb5ebb67
David Hedges <dhedges@student.neumont.edu>
2017-02-15 18:09:44 -0700

    Made Rarity and AlchymicEffects enums

---
+++ AlchymyShoppe/AlchymyShoppe/AlchymicEffects.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlchymyShoppe
+{
+    class AlchymicEffects
+    {
+        enum AlchymicEffect { Heal, Regenerate, Damage, Speed, Strength,
+            Insivibility, Charisma, Paralysis, Mana, Nightvision};
+    }
+}
---
+++ AlchymyShoppe/AlchymyShoppe/ItemRaritys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlchymyShoppe
+{
+    class ItemRaritys
+    {
+        enum Rarity { Rubbish, Inferior, Common, Uncommon, Rare, Epic, Legendary, Godlike };
+    }
+}
