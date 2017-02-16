53a3db1f2d7fd080c1421b0a997d3a1db6d29c8f
brandwand <brandwand@yahoo.com>
2017-02-15 18:14:35 -0700

    Brandon, got basics for Item, and Player classes

---
+++ AlchymyShoppe/AlchymyShoppe/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlchymyShoppe
+{
+   
+    abstract class  Item
+    {
+        public String name { get; set; }
+        public int price { get; set; }
+        public String rarity { get; set; }
+        public String effect { get; set; }
+        public Item(String name, int price, String rarity, String rffect)
+        {
+            this.name = name;
+            this.price = price;
+            this.rarity = rarity;
+            this.effect = effect;
+        }
+    }
+}
---
+++ AlchymyShoppe/AlchymyShoppe/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlchymyShoppe
+{
+    class Player
+    {
+        public String name { get; set; }
+        public int gold { get; set; }
+
+    }
+}
