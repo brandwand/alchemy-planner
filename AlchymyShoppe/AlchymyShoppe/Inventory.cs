ab01a7b11ed1c85c86f479963b46dc76fe33bc53
klee <knlee6@gmail.com>
2017-02-16 18:31:40 -0700

    Added it for real this time.

---
+++ AlchymyShoppe/AlchymyShoppe/Inventory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlchymyShoppe
+{
+    class Inventory
+    {
+        private List<> Items;
+        private int Gold;
+        public Inventory(int gold)
+        {
+            Gold = gold;
+        }
+        public Inventory(int gold, List<> ItemsForStartingInventory)
+        {
+            Gold = gold;
+            Items = ItemsForStartingInventory;
+        }
+        public Inventory(List<> ItemsForStartingInventory)
+        {
+            Items = ItemsForStartingInventory;
+        }
+        public bool StoreInventory()
+        {
+
+        }
+        public bool LoadInventory()
+        {
+
+        }
+        public void ShowInventory()
+        {
+
+        }
+    }
+}
