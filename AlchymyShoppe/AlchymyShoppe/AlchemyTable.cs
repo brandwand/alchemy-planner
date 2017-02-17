b689e56b03117349a85d322168b75db7ae333e26
brandwand <brandwand@yahoo.com>
2017-02-16 18:08:10 -0700

    Forgot Curly Braces

--- AlchymyShoppe/AlchymyShoppe/AlchemyTable.cs
+++ AlchymyShoppe/AlchymyShoppe/AlchemyTable.cs
@@ -14,11 +14,13 @@ namespace AlchymyShoppe
         Ingredient ingredient3;
         Potion potion;
 
-        public AlchemyTable(Player player, Ingredient ingredient1, Ingredient ingredient2, Ingredient ingredient3, Potion potion) {
+        public AlchemyTable(Player player, Ingredient ingredient1, Ingredient ingredient2, Ingredient ingredient3, Potion potion)
+        {
             this.player = player;
             this.ingredient1 = ingredient1;
             this.ingredient2 = ingredient2;
             this.ingredient3 = ingredient3;
             this.potion = potion;
+        }
     }
 }
