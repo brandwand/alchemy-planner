7a13b450e9de4bd1ec63c4cdde9c7abb57c2d4f6
klee <knlee6@gmail.com>
2017-02-24 18:27:04 -0700

    Started more work on saving to a file in Chest.cs

--- AlchymyShoppe/AlchymyShoppe/Managers/Chest.cs
+++ AlchymyShoppe/AlchymyShoppe/Managers/Chest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +9,36 @@ namespace AlchymyShoppe
 {
     class Chest
     {
-        //Files for whatever we may need
-        private string playerFile;
-        public bool loadGame()
+        //Hard coded names used for all files in the save folders
+        public bool loadGame(string folderLocation)
         {
 
         }
-        public bool saveGame(Player player)
-        private bool serialize(String data)
+        public bool saveGame(string folderLocation, Player currentPlayer)
         {
+            if (File.Exists(folderLocation))
+            {
+                if(savePlayer(folderLocation, currentPlayer) && saveInventory(folderLocation, currentPlayer.getInventory()))
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Save folder " + folderLocation + " does not exist.");
+            }
             return false;
+        }
+
+        private bool saveInventory(string folderLocation, Player currentPlayer)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        private bool savePlayer(string folderLocation, Player currentPlayer)
+        {
+            throw new NotImplementedException();
         }
     }
 }
