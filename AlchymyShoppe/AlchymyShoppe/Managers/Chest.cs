3d1c4e6dfe9aeb511d86690b94b831d017adfb8d
klee <knlee6@gmail.com>
2017-03-02 16:05:26 -0700

    Everything is set up but serializing and de-serializing the file data. also loadGame returns a player with his/her inventory and book already created.

--- AlchymyShoppe/AlchymyShoppe/Managers/Chest.cs
+++ AlchymyShoppe/AlchymyShoppe/Managers/Chest.cs
@@ -16,10 +16,48 @@ namespace AlchymyShoppe
         private string inventoryFile = "Inventory.txt";
         private string recipeBookFile = "RecipeBook.txt";
         private string saveDataLocation = System.IO.Path.Combine(currentDirectory, "SaveData");
-        public void loadGame(string folderLocation)
+        public Player loadGame(string folderLocation)
         {
-
+            if (Directory.Exists(folderLocation))
+            {
+                return loadPlayer(folderLocation);
+            }
+            else
+            {
+                //Default if something goes wrong reading information.
+                return new Player("Player1",250);
+            }
         }
+
+        private Player loadPlayer(string folderLocation)
+        {
+            string file = System.IO.Path.Combine(folderLocation, playerFile);
+            StreamReader sr = new StreamReader(file);
+            string rawData = sr.ReadToEnd();
+            string playerName = "";
+            string gold = "";
+            deserializePlayer(rawData, out playerName, out gold);
+            Player p = new Player(playerName, Int32.Parse(gold));
+            p.setInventory(loadInventory());
+            p.setPlayerBook(loadBook());
+            return p;
+        }
+
+        private Inventory loadInventory()
+        {
+            throw new NotImplementedException();
+        }
+
+        private RecipeBook loadBook()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void deserializePlayer(string rawData, out string playerName, out string gold)
+        {
+            throw new NotImplementedException();
+        }
+
         public void saveGame(string folderLocation, Player currentPlayer)
         {
             if (Directory.Exists(folderLocation))
@@ -41,38 +79,6 @@ namespace AlchymyShoppe
                 saveGame(folderLocation, currentPlayer);
             }
         }
-
-        private void saveRecipeBook(string folderLocation, RecipeBook currentBook)
-        {
-            string[] bookContents = serializeRecipeBook(currentBook);
-            string file = folderLocation + "/" + recipeBookFile;
-            StreamWriter writer = new StreamWriter(file);
-            foreach (string line in bookContents)
-            {
-                writer.WriteLine(line);
-            }
-        }
-
-        private string[] serializeRecipeBook(RecipeBook currentBook)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void saveInventory(string folderLocation, Inventory currentInventory)
-        {
-            string[] inventoryContents = serializeInventory(currentInventory);
-            string file = folderLocation + "/" + inventoryFile;
-            StreamWriter writer = new StreamWriter(file);
-            foreach(string line in inventoryContents){
-                writer.WriteLine(line);
-            }
-        }
-
-        private string[] serializeInventory(Inventory currentInventory)
-        {
-            throw new NotImplementedException();
-        }
-
         private void savePlayer(string folderLocation, Player currentPlayer)
         {
             string[] playerContents = serializePlayer(currentPlayer);
@@ -83,8 +89,34 @@ namespace AlchymyShoppe
                 writer.WriteLine(line);
             }
         }
-
+        private void saveRecipeBook(string folderLocation, RecipeBook currentBook)
+        {
+            string[] bookContents = serializeRecipeBook(currentBook);
+            string file = folderLocation + "/" + recipeBookFile;
+            StreamWriter writer = new StreamWriter(file);
+            foreach (string line in bookContents)
+            {
+                writer.WriteLine(line);
+            }
+        }
+        private void saveInventory(string folderLocation, Inventory currentInventory)
+        {
+            string[] inventoryContents = serializeInventory(currentInventory);
+            string file = folderLocation + "/" + inventoryFile;
+            StreamWriter writer = new StreamWriter(file);
+            foreach(string line in inventoryContents){
+                writer.WriteLine(line);
+            }
+        }
         private string[] serializePlayer(Player currentPlayer)
+        {
+            throw new NotImplementedException();
+        }
+        private string[] serializeRecipeBook(RecipeBook currentBook)
+        {
+            throw new NotImplementedException();
+        }
+        private string[] serializeInventory(Inventory currentInventory)
         {
             throw new NotImplementedException();
         }
