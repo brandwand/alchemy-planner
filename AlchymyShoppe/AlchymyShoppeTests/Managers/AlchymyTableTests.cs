16fde8c79447dcaddd991cd4c3708d57214ffb12
Laxdyn <dterry@student.neumont.edu>
2017-03-07 19:10:33 -0700

    Combined test classes so only one exists and has 1 test playlist. All flags have been changed from binary to numeric values for flags(1,2,4,8,16,32,etc)

--- AlchymyShoppe/AlchymyShoppeTests/Managers/AlchymyTableTests.cs
+++ AlchymyShoppe/AlchymyShoppeTests/Managers/AlchymyTableTests.cs
@@ -63,6 +63,28 @@ namespace AlchymyShoppe.Tests
             //result0010000100010000
             Assert.AreEqual(Rarity.Godlike, table.Potion.rarity);
         }
+        [TestMethod()]
+        public void addGoldTest()
+        {
+            AlchymyShoppe.Models.AlchymyShoppe alchShoppe = new AlchymyShoppe.Models.AlchymyShoppe();
+        
+            AlchymyShoppe.Models.Player player = new AlchymyShoppe.Models.Player("Bill", 456);
+            AlchymyShoppe.Models.Rarity rarity = new AlchymyShoppe.Models.Rarity();
+            AlchymyShoppe.Models.AlchymicEffect effect = new AlchymyShoppe.Models.AlchymicEffect();
+
+            Ingredient ingredient1 = new Ingredient("Flaming Tail", "beast.png", 700, Rarity.Uncommon, (AlchymicEffect)8448); //0010000100000000
+            Ingredient ingredient2 = new Ingredient("Koro Tentacle", "fish.png", 3500, Rarity.Godlike, (AlchymicEffect)12560);//0011000100010000
+            Ingredient ingredient3 = new Ingredient("Twisted Root", "plant.png", 200, Rarity.Inferior, (AlchymicEffect)30);   //0000000000011110
+            List<Ingredient> ingredients = new List<Ingredient>();
+
+            ingredients.Add(ingredient1);
+            ingredients.Add(ingredient2);
+            ingredients.Add(ingredient3);
+
+            AlchymyShoppe.Models.Potion potion = new Potion("PotionX", "", 200, rarity, ingredients, effect);
+            alchShoppe.addPlayerGold(player, potion);
+            Assert.IsTrue(player.Gold == 256, "Failed To Add");
+        }
 
         [TestMethod()]
         public void priceTest()
@@ -101,5 +123,7 @@ namespace AlchymyShoppe.Tests
         //        Assert.AreEqual(true, ingredientsWithName.Count() >= 1);
         //}
     }
+
     
+
 }
